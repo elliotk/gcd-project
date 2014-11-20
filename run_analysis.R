bafacb5d0760fb81bb6bbf4fe6a9da263c1dcd53
Elliot Kleiman <elliotkleiman@gmail.com>
2014-11-19 22:35:27 -0500

    Add extract mean and std measurements and label activity names

--- run_analysis.R
+++ run_analysis.R
@@ -2,7 +2,7 @@
 # run_analysis.R
 # Wed Nov 19 17:45:32 EST 2014 
 
-# Download data ---------------------------
+# 0. Download data ---------------------------
 
 # Data dir exist?
 if (!file.exists("data")) {
@@ -30,42 +30,71 @@ dateDownloaded <- date()
 # Extract zip archive file
 unzip(dataFile, exdir = dataDirectory) 
 
-# Merge data ---------------------------
+# 1. Merge data ---------------------------
 
-# Set dataset dir
+# Set dataset directory
 datasetDirectory <- file.path(dataDirectory, "UCI\ HAR\ Dataset")
 
-# Set training dataset dir
+# Set training dataset directory
 trainingDirectory <- file.path(datasetDirectory, "train")
 
 # Set training dataset files
-trainingSet <- file.path(trainingDirectory, "X_train.txt")
-trainingSubjects <- file.path(trainingDirectory, "subject_train.txt")
-trainingLabels <- file.path(trainingDirectory, "y_train.txt")
+trainingFile         <- file.path(trainingDirectory, "X_train.txt")
+trainingSubjectsFile <- file.path(trainingDirectory, "subject_train.txt")
+trainingLabelsFile   <- file.path(trainingDirectory, "y_train.txt")
 
 # Read training set
-train.set <- read.table(trainingSet)
-train.subj <- read.table(trainingSubjects)
-train.labs <- read.table(trainingLabels)
+train.set  <- read.table(trainingFile)
+train.subj <- read.table(trainingSubjectsFile)
+train.labs <- read.table(trainingLabelsFile)
 
 # Merge training set
 train.merge <- cbind(train.set, train.subj, train.labs)
 
-# Set test dataset dir
+# Set test dataset directory
 testDirectory <- file.path(datasetDirectory, "test")
 
 # Set test dataset files
-testSet <- file.path(testDirectory, "X_test.txt")
-testSubjects <- file.path(testDirectory, "subject_test.txt")
-testLabels <- file.path(testDirectory, "y_test.txt")
+testFile         <- file.path(testDirectory, "X_test.txt")
+testSubjectsFile <- file.path(testDirectory, "subject_test.txt")
+testLabelsFile   <- file.path(testDirectory, "y_test.txt")
 
 # Read test set
-test.set <- read.table(testSet)
-test.subj <- read.table(testSubjects)
-test.labs <- read.table(testLabels)
+test.set  <- read.table(testFile)
+test.subj <- read.table(testSubjectsFile)
+test.labs <- read.table(testLabelsFile)
 
 # Merge test set
 test.merge <- cbind(test.set, test.subj, test.labs)
 
 # Merge training and test sets to create one complete data set
 complete.set <- rbind(train.merge, test.merge)
+
+# 2. Extract mean and std measurements  ---------------------------
+
+# Set features file
+featuresFile <- file.path(datasetDirectory, "features.txt")
+
+# Read features file
+features <- read.table(featuresFile, stringsAsFactors = FALSE)
+
+# Get mean indices
+mean.features <- grep("mean\\(\\)", features$V2, value = FALSE)
+
+# Get std indices
+std.features <- grep("std\\(\\)", features$V2, value = FALSE)
+
+# Extract only measurements on mean and std, and keep the last two columns
+# of subject and activity
+reduced.set <- complete.set[, c(mean.features, std.features, 562, 563)]
+
+# 3. Use descriptive activity names to name the activities ---------------------------
+
+# Set activity_labels file
+activityLabelsFile <- file.path(datasetDirectory, "activity_labels.txt")
+
+# Read activity_lablels file
+activity.labels <- read.table(activityLabelsFile, stringsAsFactors = FALSE)
+
+# Set activity names from activity labels to activity column
+reduced.set[, 68] <- factor(reduced.set[, 68], labels = activity.labels$V2)
