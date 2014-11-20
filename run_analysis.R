2e4bc472a7ce81d0157474e33bdf7a969886e374
Elliot Kleiman <elliotkleiman@gmail.com>
2014-11-19 20:44:09 -0500

    Add merge train and test sets

--- run_analysis.R
+++ run_analysis.R
@@ -33,8 +33,39 @@ unzip(dataFile, exdir = dataDirectory)
 # Merge data ---------------------------
 
 # Set dataset dir
-datasetDirectory <- "UCI\ HAR\ Dataset"
+datasetDirectory <- file.path(dataDirectory, "UCI\ HAR\ Dataset")
 
-# Set path/to/dataset directory
-datasetPath <- file.path(dataDirectory, datasetDirectory)
+# Set training dataset dir
+trainingDirectory <- file.path(datasetDirectory, "train")
 
+# Set training dataset files
+trainingSet <- file.path(trainingDirectory, "X_train.txt")
+trainingSubjects <- file.path(trainingDirectory, "subject_train.txt")
+trainingLabels <- file.path(trainingDirectory, "y_train.txt")
+
+# Read training set
+train.set <- read.table(trainingSet)
+train.subj <- read.table(trainingSubjects)
+train.labs <- read.table(trainingLabels)
+
+# Merge training set
+train.merge <- cbind(train.set, train.subj, train.labs)
+
+# Set test dataset dir
+testDirectory <- file.path(datasetDirectory, "test")
+
+# Set test dataset files
+testSet <- file.path(testDirectory, "X_test.txt")
+testSubjects <- file.path(testDirectory, "subject_test.txt")
+testLabels <- file.path(testDirectory, "y_test.txt")
+
+# Read test set
+test.set <- read.table(testSet)
+test.subj <- read.table(testSubjects)
+test.labs <- read.table(testLabels)
+
+# Merge test set
+test.merge <- cbind(test.set, test.subj, test.labs)
+
+# Merge training and test sets to create one complete data set
+complete.set <- rbind(train.merge, test.merge)
