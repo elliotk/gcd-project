ac5477657fd378bd6c34459b9533d3487e87dbcd
Elliot Kleiman <elliotkleiman@gmail.com>
2014-11-19 19:23:21 -0500

    Add download the dataset section

--- run_analysis.R
+++ run_analysis.R
@@ -2,6 +2,8 @@
 # run_analysis.R
 # Wed Nov 19 17:45:32 EST 2014 
 
+# Download data ---------------------------
+
 # Data dir exist?
 if (!file.exists("data")) {
     dir.create("data")
@@ -10,13 +12,29 @@ if (!file.exists("data")) {
 # Set File Url
 fileUrl <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
 
-# Download file
-download.file(fileUrl, destfile = "./data/dataset.zip", method = "curl")
+# Set data dir
+dataDirectory <- "./data"
 
-# Record data downloaded
+# Set data file name
+fileName <- "dataset.zip"
+
+# Set path/to/data file 
+dataFile <- file.path(dataDirectory, fileName)
+
+# Download file
+download.file(fileUrl, destfile = dataFile, method = "curl")
+
+# Record date downloaded
 dateDownloaded <- date()
 
 # Extract zip archive file
-unzip("./data/dataset.zip", exdir = "./data") 
+unzip(dataFile, exdir = dataDirectory) 
 
+# Merge data ---------------------------
+
+# Set dataset dir
+datasetDirectory <- "UCI\ HAR\ Dataset"
+
+# Set path/to/dataset directory
+datasetPath <- file.path(dataDirectory, datasetDirectory)
 
