b1cab71fb16eddb95bc37506dd773c6b3be1c5a7
Elliot Kleiman <elliotkleiman@gmail.com>
2014-11-22 20:43:02 -0500

    Update formatting, naming, and Add parts 4 and 5

--- run_analysis.R
+++ run_analysis.R
@@ -2,7 +2,7 @@
 # run_analysis.R
 # Wed Nov 19 17:45:32 EST 2014 
 
-# 0. Download data ---------------------------
+## 0. Download data ---------------------------
 
 # Data dir exist?
 if (!file.exists("data")) {
@@ -10,73 +10,73 @@ if (!file.exists("data")) {
 }
 
 # Set File Url
-fileUrl <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
+file.url <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
 
 # Set data dir
-dataDirectory <- "./data"
+data.directory <- "./data"
 
 # Set data file name
-fileName <- "dataset.zip"
+file.name <- "dataset.zip"
 
 # Set path/to/data file 
-dataFile <- file.path(dataDirectory, fileName)
+data.file <- file.path(data.directory, file.name)
 
 # Download file
-download.file(fileUrl, destfile = dataFile, method = "curl")
+download.file(file.url, destfile = data.file, method = "curl")
 
 # Record date downloaded
-dateDownloaded <- date()
+date.downloaded <- date()
 
 # Extract zip archive file
 unzip(dataFile, exdir = dataDirectory) 
 
-# 1. Merge data ---------------------------
+## 1. Merge data ---------------------------
 
 # Set dataset directory
-datasetDirectory <- file.path(dataDirectory, "UCI\ HAR\ Dataset")
+dataset.directory <- file.path(data.directory, "UCI\ HAR\ Dataset")
 
-# Set training dataset directory
-trainingDirectory <- file.path(datasetDirectory, "train")
+# Set training data directory
+training.directory <- file.path(dataset.directory, "train")
 
-# Set training dataset files
-trainingFile         <- file.path(trainingDirectory, "X_train.txt")
-trainingSubjectsFile <- file.path(trainingDirectory, "subject_train.txt")
-trainingLabelsFile   <- file.path(trainingDirectory, "y_train.txt")
+# Set training data files
+training.file          <- file.path(training.directory, "X_train.txt")
+training.subjects.file <- file.path(training.directory, "subject_train.txt")
+training.labels.file   <- file.path(training.directory, "y_train.txt")
 
-# Read training set
-train.set  <- read.table(trainingFile)
-train.subj <- read.table(trainingSubjectsFile)
-train.labs <- read.table(trainingLabelsFile)
+# Read training data
+train.data <- read.table(training.file)
+train.subj <- read.table(training.subjects.file)
+train.labs <- read.table(training.labels.file)
 
-# Merge training set
-train.merge <- cbind(train.set, train.subj, train.labs)
+# Merge training data
+train.merge <- cbind(train.data, train.subj, train.labs)
 
-# Set test dataset directory
-testDirectory <- file.path(datasetDirectory, "test")
+# Set test data directory
+test.directory <- file.path(dataset.directory, "test")
 
-# Set test dataset files
-testFile         <- file.path(testDirectory, "X_test.txt")
-testSubjectsFile <- file.path(testDirectory, "subject_test.txt")
-testLabelsFile   <- file.path(testDirectory, "y_test.txt")
+# Set test data files
+test.file          <- file.path(test.directory, "X_test.txt")
+test.subjects.file <- file.path(test.directory, "subject_test.txt")
+test.labels.file   <- file.path(test.directory, "y_test.txt")
 
-# Read test set
-test.set  <- read.table(testFile)
-test.subj <- read.table(testSubjectsFile)
-test.labs <- read.table(testLabelsFile)
+# Read test data
+test.data <- read.table(test.file)
+test.subj <- read.table(test.subjects.file)
+test.labs <- read.table(test.labels.file)
 
-# Merge test set
-test.merge <- cbind(test.set, test.subj, test.labs)
+# Merge test data
+test.merge <- cbind(test.data, test.subj, test.labs)
 
-# Merge training and test sets to create one complete data set
-complete.set <- rbind(train.merge, test.merge)
+# Merge training and test data
+training.test <- rbind(train.merge, test.merge)
 
-# 2. Extract mean and std measurements  ---------------------------
+## 2. Extract mean and std measurements  ---------------------------
 
 # Set features file
-featuresFile <- file.path(datasetDirectory, "features.txt")
+features.file <- file.path(dataset.directory, "features.txt")
 
-# Read features file
-features <- read.table(featuresFile, stringsAsFactors = FALSE)
+# Read features file to get variable names
+features <- read.table(features.file, stringsAsFactors = FALSE)
 
 # Get mean indices
 mean.features <- grep("mean\\(\\)", features$V2, value = FALSE)
@@ -84,17 +84,41 @@ mean.features <- grep("mean\\(\\)", features$V2, value = FALSE)
 # Get std indices
 std.features <- grep("std\\(\\)", features$V2, value = FALSE)
 
-# Extract only measurements on mean and std, and keep the last two columns
-# of subject and activity
-reduced.set <- complete.set[, c(mean.features, std.features, 562, 563)]
+# Extract columns for mean and std, and subject and activity
+mean.std <- training.test[, c(mean.features, std.features, 562, 563)]
 
-# 3. Use descriptive activity names to name the activities ---------------------------
+## 3. Use descriptive activity names to name the activities ---------------------------
 
 # Set activity_labels file
-activityLabelsFile <- file.path(datasetDirectory, "activity_labels.txt")
+activity.labels.file <- file.path(dataset.directory, "activity_labels.txt")
 
-# Read activity_lablels file
-activity.labels <- read.table(activityLabelsFile, stringsAsFactors = FALSE)
+# Read activity_labels file
+activity.labels <- read.table(activity.labels.file, stringsAsFactors = FALSE)
 
-# Set activity names from activity labels to activity column
-reduced.set[, 68] <- factor(reduced.set[, 68], labels = activity.labels$V2)
+# Set activity column values to activity names from activity labels
+mean.std[, 68] <- factor(mean.std[, 68], labels = activity.labels$V2)
+
+## 4. Label data with descriptive variable names  ---------------------------
+
+# Mean feature names:
+# Remove parenthesis
+mean.features.clean <- sub('\\(\\)', '', features$V2[mean.features], perl = TRUE)
+# Replace - with .
+mean.features.clean <- gsub('-', '.', mean.features.clean)
+
+# Std feature names:
+# Remove parenthesis
+std.features.clean <- sub('\\(\\)', '', features$V2[std.features], perl = TRUE)
+# Replace - with .
+std.features.clean <- gsub('-', '.', std.features.clean)
+
+
+                            
+#aggdata <- aggregate(reduced.set, by = list(Activity = reduced.set$V1.2, Subject = reduced.set$V1.1), FUN = mean, na.rm = TRUE)
+#summary(subset(aggdata, select = -c(V1.1, V1.2))) # or, summary(aggdata[-c(69, 70)])
+
+aggdata.2 <- reduced.set %>%
+               group_by(V1.2, V1.1) %>%
+               summarise_each(funs(mean))
+
+write.table(aggdata.2, row.names = FALSE, file = "./data/dplyr.result.txt")
